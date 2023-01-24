a4849ef9febcc31014c5ec54b53c21ef96d71dbb
Lakshay <lakshayyadav2006@gmail.com>
2023-01-24 15:13:47 +0530

    changed the data fetch url.

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
 	// fetch data from server
 
-	const response = await fetch("http://localhost:5000", {
+	const response = await fetch("https://jarvis-gpt3-l7r7.onrender.com/", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json"
