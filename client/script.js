22ecb4c2795bc9e5535db00cabdbdec0ff245e88
Lakshay <lakshayyadav2006@gmail.com>
2023-01-24 15:28:46 +0530

    reverted.

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
 	// fetch data from server
 
-	const response = await fetch(process.env.FETCH_URL, {
+	const response = await fetch("https://jarvis-gpt3-l7r7.onrender.com/", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json"
